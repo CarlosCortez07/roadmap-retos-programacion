cf1f59c1adb4b2d8242b6ceccf3653821a3b0876
Carlos <carloscortezmarrero@gmail.com>
2025-11-12 12:53:39 +0100

    Ejercicio 00 CARLOS CORTEZ.

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/c#/cortez97.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+        // URL: https://learn.microsoft.com/en-us/dotnet/csharp/
+
+        // Una línea en C#
+        /*
+         Varias 
+         líneas
+         en
+         C#
+         */
+        var string saludo= "Hola";
+        const string otrosaludo = "Hola";
+        int a = 2;
+        float b = 2.3;
+        double c = 234.566;
+        decimal d = 0.2;
+        bool e = false;
+        bool f = true;
+    
+    
+    }
+
+    public static void main(string[] args) 
+    {
+
+        Console.WriteLine("Hola, C#");
+
+    }
+}
