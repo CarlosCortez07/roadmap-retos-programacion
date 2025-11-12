20236cdfb1878d8f283cd5f59a5787f3636a50c5
CarlosCortez07 <100127383+CarlosCortez07@users.noreply.github.com>
2025-11-12 12:28:10 +0100

    Create cortez.cs

---
+++ cortez.cs
@@ -0,0 +1,25 @@
+using system; 
+
+1 
+  // URL: https://learn.microsoft.com/en-us/dotnet/csharp/ 
+2 
+/*
+Ejemplo
+varias
+líneas
+*/
+3 
+ var string ="Hola";  
+const saludo = "hola";
+4
+ var string="Hola";
+ var int=3;
+ var bool= true; 
+ var double= 3.5; 
+5
+
+public static void main(String[] args)
+{
+  string lenguaje= "C#";
+    Console.WriteLine($"Hola, {lenguaje}"); 
+}
